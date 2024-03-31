7e42b5eedfa202c0eb4b2871a84b7a8f68d145a3
ivineettiwari <tiwarivineet1998@gmail.com>
2024-03-31 15:47:41 +0530

    Script changes

--- django_setup/blogforum/static/scripts.js
+++ django_setup/blogforum/static/scripts.js
@@ -5,19 +5,23 @@ function callAPI(){
     fetch('http://127.0.0.1:8000/data')
         .then(response => response.json())
         .then(users => {
-            users = JSON.parse(users.data);
-            // Iterate through the list of users and create a card for each
-            users.forEach(user => {
-                const userCard = document.createElement("li");
-                userCard.className = "userCard";
-                userCard.innerHTML = `
-                    <h2>${user.name}</h2>
-                    <p><strong>Email:</strong> ${user.email}</p>
-                    <p><strong>Username:</strong> ${user.username}</p>
-                    <p><strong>Phone:</strong> ${user.phone}</p>
-                `;
-                userListElement.appendChild(userCard);
-            });
+            try{
+                users = JSON.parse(users.data);
+                // Iterate through the list of users and create a card for each
+                users.forEach(user => {
+                    const userCard = document.createElement("li");
+                    userCard.className = "userCard";
+                    userCard.innerHTML = `
+                        <h2>${user.name}</h2>
+                        <p><strong>Email:</strong> ${user.email}</p>
+                        <p><strong>Username:</strong> ${user.username}</p>
+                        <p><strong>Phone:</strong> ${user.phone}</p>
+                    `;
+                    userListElement.appendChild(userCard);
+                });
+            }catch(e){
+                
+            }
         })
         .catch(error => {
             console.error('Error fetching user data:', error);
